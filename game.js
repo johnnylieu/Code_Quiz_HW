2c77156028e3d73d4510a7728eeff4282b0c4536
Johnny Lieu <johnnylieu@yahoo.com>
2020-07-26 15:44:52 -0700

    added event listener for click

--- game.js
+++ game.js
@@ -65,4 +65,16 @@ getNewQuestion = () => {
     acceptingAnswers = true;
 };
 
+choices.forEach(choice =>{
+    choice.addEventListener('click', e => {
+    if(!acceptingAnswers) return;
+
+    acceptingAnswers = false;
+    const selectedChoice = e.target;
+    const selectedAnswer = selectedChoice.dataset['number'];
+    
+    getNewQuestion ();
+    });
+});
+
 startGame();
