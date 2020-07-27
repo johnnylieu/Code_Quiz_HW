faec72f9147ffd985dbf9c7ba165be43c73eca51
Johnny Lieu <johnnylieu@yahoo.com>
2020-07-26 20:10:38 -0700

    sort scores

--- end.js
+++ end.js
@@ -2,6 +2,9 @@ const username = document.getElementById('username');
 const saveScoreBtn = document.getElementById('saveScoreBtn');
 const finalScore = document.getElementById('finalScore');
 const mostRecentScore = localStorage.getItem('mostRecentScore');
+
+const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
+
 finalScore.innerText = mostRecentScore;
 
 username.addEventListener("keyup", () => {
@@ -10,4 +13,8 @@ saveScoreBtn.disabled = !username.value;
 
 saveHighScore = e => {
 e.preventDefault();
+const score = {score: mostRecentScore, name: username.value};
+highScores.push(score);
+
+highScores.sort( (a,b) => b.score - a.score);
 };
